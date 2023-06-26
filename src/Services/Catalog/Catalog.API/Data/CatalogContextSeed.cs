c9e97616f04c5f01a8796b1113f4e35b55f59699
rubenmontiel <rubenmontiwl@gmail.com>
2023-06-26 12:42:57 +0000

    Adding Mongo Database Connection

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public CatalogContextSeed()
+        {
+
+        }
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+
+        }
+    }
+}
